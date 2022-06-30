acdbad392f7257313234139291046ef5c828544e
Sanjay Gora <sanjaygora@cwebconsultants.com>
2022-06-30 13:35:54 +0530

    new changes

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -70,31 +70,11 @@
             </div>
         </div>
         <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-
-                        <li>
-                            <a href="">
-                                Contact
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                Blog
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                Licenses
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+            <div class="container-fluid">                
                 <div class="copyright pull-right">
                     &copy;
                     <script>document.write(new Date().getFullYear())</script>
-                    , made with <i class="fa fa-heart heart"></i> by <a href="">Javed</a>
+                    , made with <i class="fa fa-heart heart"></i> by <a href="">Laravel</a>
                 </div>
             </div>
         </footer>
