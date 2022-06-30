b5dace92be9e36248da6dde3ad8d0d5df8c95d3b
Sanjay Gora <sanjaygora@cwebconsultants.com>
2022-06-30 13:36:41 +0530

    new changes

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -69,16 +69,6 @@
 
             </div>
         </div>
-        <footer class="footer">
-            <div class="container-fluid">                
-                <div class="copyright pull-right">
-                    &copy;
-                    <script>document.write(new Date().getFullYear())</script>
-                    , made with <i class="fa fa-heart heart"></i> by <a href="">Laravel</a>
-                </div>
-            </div>
-        </footer>
-
     </div>
 </div>
 
