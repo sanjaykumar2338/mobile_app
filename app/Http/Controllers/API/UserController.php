7101c808c05eeac2a1271fc20fa766d2e6401711
Sanjay Gora <sanjaygora@cwebconsultants.com>
2022-07-02 15:07:40 +0530

    fixed api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -40,7 +40,7 @@ class UserController extends Controller
         ]);
         
         if ($validator->fails()) { 
-            return response()->json(['error'=>$validator->errors()], 401);            
+            return response()->json(['isSuccess'=>false,'data'=>$validator->errors()], 401);            
         }
 
         $input = $request->all(); 
